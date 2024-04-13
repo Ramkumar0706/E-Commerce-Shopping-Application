f351f62f5add593b30f80d409368c74e1d543f61
Ramkumar0706 <sakthiram2677@gmail.com>
2024-04-13 11:46:11 +0530

    HeaderUpdatedCompleted

--- E-Commerce-Application-UI/src/App.jsx
+++ E-Commerce-Application-UI/src/App.jsx
@@ -5,9 +5,10 @@ import {Outlet} from 'react-router-dom'
 const App = () => {
   return (
   <div>
-    <Header/>
+    <Header
+    />
     <div>
-    <Outlet/>
+    <Outlet/> 
     </div>
     </div>
   )
